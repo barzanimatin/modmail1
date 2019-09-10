c3609380f0a36f0c360657d9a256a191543d45ae
Reaper Gamer <39646826+biswajit9123@users.noreply.github.com>
2019-09-10 14:41:25 +0530

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -236,6 +236,17 @@ class Utility:
         embed.set_footer(text=footer)
         await ctx.send(embed=embed)
 
+    @debug.command()
+    @commands.is_owner()
+    @trigger_typing        
+    async def say(ctx, *, msg = None):
+    await client.delete_message(ctx.message)
+    if ctx.message.author.bot:
+      return
+    else:
+      if not msg: await client.say("Please specify a message to send")
+      else: await client.say(msg)
+                          
     @commands.group()
     @commands.is_owner()
     @trigger_typing
